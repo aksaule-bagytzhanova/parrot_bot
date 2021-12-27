581ed40205100b789bbf0392f17b62ace1692a33
Aksi <aksaule124026@gmail.com>
2021-12-27 21:21:49 +0600

    My parrot bot

---
+++ main.py
@@ -0,0 +1,18 @@
+import telebot
+from config import TOKEN
+from stop_words_logic import stop_logic
+
+
+bot = telebot.TeleBot(token=TOKEN)
+
+@bot.message_handler(content_types=["text"])
+def print_parrot(message):
+    if stop_logic(message.text):
+        bot.delete_message(message.chat.id, message.message_id)
+        bot.send_message(message.chat.id, 'Не пишите такие слова')
+    else:
+        bot.send_message(message.chat.id, message.text)
+
+
+if __name__ == '__main__':
+    bot.polling(none_stop=True)
